be9cd0c57980c6ca98f1ed52e46c1096fcaac41e
Richard DESNOUES <70509067+Richarddesnoues@users.noreply.github.com>
2022-08-25 17:07:38 +0200

    intégration de l'image de présentation et proble avec le texte  à l'interieur

--- views/header.tpl.php
+++ views/header.tpl.php
@@ -1,13 +1,41 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="./assets/css/index.css">
     <title>Feat*</title>
 </head>
 
 <body>
-    <h1>Header</h1>
-    
+    <div class="header">
+        <div class="logo">
+            <a href='#'><img src="./assets/img/feat-logo-254x87-fluo.png" alt="logo du site"></a>
+        </div>
+        <div class="navbar">
+            <ul class="nav_items">
+                <li><a href="#">Gobelets</a></li>
+                <li><a href='#'>Balisage</a></li>
+                <li><a href='#'>Matériel évenementiel</a></li>
+                <li><a href='#'>Déstockage</a></li>
+
+            </ul>
+        </div>
+        <div class="connect_cart">
+            <div class="login">
+                <span>logo</span>
+                <a href='#'>
+                    <p>Connexion</p>
+                </a>
+            </div>
+            <div class="cart">
+                <a href='#'>
+                    <p>panier</p>
+                </a>
+            </div>
+
+        </div>
+
+    </div>
--- views/home.tpl.php
+++ views/home.tpl.php
@@ -1,3 +1,14 @@
-<div class="homePage">
-    <h1>Home</h1>
-</div>
+<section class="presentation">
+    <div class="img_home">
+        <img src="./assets/img/bcg_home.webp" width=100% height="50%" alt="photo d'une motocross dans les airs">
+    </div>
+
+    <div class="site_name">
+        <div class="feat">
+            <p>FEAT -MX-S - FEAT.MX</p>
+        </div>
+        <div class="motocross">
+            <p>Motocross Enduro Fmx Freeride et autres sports alternatifs</p>
+        </div>
+    </div>
+</section>
